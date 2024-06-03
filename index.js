cff491307b3345c94aef219b1794a5ba8791dff6
Sebastian <sebitas5225@gmail.com>
2024-06-02 23:46:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, './css')));
 
 app.use('/graphql', createHandler({ schema, rootValue: resolvers }));
 
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
+app.get('/graphiql', expressPlayground({ endpoint: '/graphql' }));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
